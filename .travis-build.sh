393fe37a5baf66601afd19ea40cebc2efeb98382
Eric Steinborn <esteinborn@gmail.com>
2014-03-11 23:24:28 -0400

    Too much whitespace.

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,10 +4,10 @@ AUTHOR=$(git show -p | grep Author)
 # Check to see if this is a pull request if so skip the test because it was ran during the initial commit.
 if [ "$AUTHOR" != "Author: Travis-CI <travis@travis-ci.org>" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-  echo -e "Starting to update GitHub Pages\n\n"
+  echo -e "Starting to update GitHub Pages\n"
 
   #copy data we're interested in to other place
-  echo -e "Copying new files to a temp folder\n\n"
+  echo -e "Copying new files to a temp folder\n"
   cp -R demo $HOME/demo
 
   #go to home and setup git
@@ -16,20 +16,20 @@ if [ "$AUTHOR" != "Author: Travis-CI <travis@travis-ci.org>" -a "$TRAVIS_PULL_RE
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  echo -e "Cloning repo\n\n"
+  echo -e "Cloning repo\n"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/esteinborn/jquery-listnav.git  gh-pages > /dev/null
 
   #go into directory and copy data we're interested in to that directory
 
-  echo -e "Entering repo folder\n\n"
+  echo -e "Entering repo folder\n"
   cd gh-pages
 
-  echo -e "Deleting all existing repo content except for .git folder\n\n"
+  echo -e "Deleting all existing repo content except for .git folder\n"
   # The next line came from Stack Overflow, love that site!
   # http://stackoverflow.com/a/22340057/682288
   find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 
-  echo -e "Copying new files into repo\n\n"
+  echo -e "Copying new files into repo\n"
   cp -Rf $HOME/demo/* .
 
   #add, commit and push files
